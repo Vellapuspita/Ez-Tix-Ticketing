9512d3e5f838688299b870559a2af88a430d09ef
Vella Puspita <vellapuspita0@gmail.com>
2025-12-08 00:30:03 +0800

    feat: replace check icon with success image in PaymentSuccessPage for enhanced visual feedback

--- frontend/src/pages/PaymentSuccessPage.jsx
+++ frontend/src/pages/PaymentSuccessPage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import image from "../assets/image.png"; // ← Import gambar ceklis kamu
 
 export default function PaymentSuccessPage() {
   const navigate = useNavigate();
@@ -6,17 +7,24 @@ export default function PaymentSuccessPage() {
   return (
     <div className="flex items-center justify-center">
       <div className="bg-white rounded-3xl shadow-card px-10 py-12 max-w-md w-full text-center">
+
+        {/* CHECK IMAGE */}
         <div className="mx-auto mb-6 h-16 w-16 rounded-full border-4 border-green-500 flex items-center justify-center">
-          <span className="material-icons-outlined text-4xl text-green-500">
-            check
-          </span>
+          <img
+            src={image}
+            alt="success"
+            className="w-30 h-30 object-contain"
+          />
         </div>
+
         <h1 className="text-xl font-semibold mb-2">
           Pembayaran berhasil
         </h1>
+
         <p className="text-sm text-muted mb-6">
           Terimakasih telah menggunakan layanan EzTix
         </p>
+
         <button
           onClick={() => navigate("/tickets")}
           className="px-6 py-2.5 rounded-full bg-primary text-white text-sm font-semibold"
